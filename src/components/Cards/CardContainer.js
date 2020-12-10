f8babb15b864f1f626c066d371fa0117cec2b793
Ziyodulla <zieu2000@gmail.com>
2020-12-10 11:53:01 +0500

    typo fix

--- src/components/Cards/CardContainer.js
+++ src/components/Cards/CardContainer.js
@@ -8,7 +8,7 @@ const CardContainer = ({ title, data }) => {
       <div className="cards">
         <div className="d-flex justify-content-between align-items-end mb-3">
           <h2 className="cards-title">{title}</h2>
-          <button className="button button--white">Se all</button>
+          <button className="button button--white nav-link">See all</button>
         </div>
         <div className="card-container row">
           {data.map((product, index) => (
